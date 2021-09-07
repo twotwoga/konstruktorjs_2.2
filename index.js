fc2f075f13923cf3b277e1353ff03bca4a364154
Gavrill <68950346+twotwoga@users.noreply.github.com>
2021-09-07 16:33:11 +0500

    Gavril
    Work

--- index.js
+++ index.js
@@ -4,22 +4,67 @@ const model = [
     {type: 'colums', value: [
         '111111',
         '222222',
-        '333333'
-    ]}
+        '333333',
+        '4444444'
+    ]},
+    {type: 'image', value: './assets/image.png'}
 ]
 
 const $site = document.querySelector('#site')
 
 model.forEach(block => {
     let html = ''
+
     if (block.type === 'title') {
-        html = `
-             <div class="row">
+        html = title(block)
+
+    } else if (block.type === 'text') {
+        html = text(block)
+    } else if (block.type === 'colums') {
+        html = colums(block)
+    } else if (block.type === 'image') {
+        html = image(block)
+    }
+
+    $site.insertAdjacentHTML('beforeend', html)
+})
+
+function title(block) {
+    return `
+            <div class="row">
+            <div class="col-sm">
+                <h1>${block.value}</h1>
+            </div>
+            </div>
+            `
+}
+
+function text(block) {
+    
+    return `
+            <div class="row">
                 <div class="col-sm">
-                    <h1>Hello World</h1>
+                    <p>${block.value}</p>
                 </div>
             </div>
-        `
+            `    
+}
 
-    }
-})
+function colums(block) {
+    const html = block.value.map(item => `<div class="col-sm">${item}</div>`)    
+    return `
+            <div class="row">
+                ${html.join('')}
+            </div>
+            `
+    
+}
+
+function image(block) {
+    return `
+           <div class="row">
+               <img src="${block.value}">
+           </div>
+           `
+    
+}
