42f76c4a5b42a3f2f489d5c094610d98a23726d9
Gavrill <68950346+twotwoga@users.noreply.github.com>
2021-09-08 12:13:47 +0500

    Gavril
    Work

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 const model = [
-    {type: 'title', value: 'Hello World from JS'},
+    {type: 'title', value: 'Hello World from JS!!!!!!'},
     {type: 'text', value: 'here we go with some text'},
     {type: 'colums', value: [
         '111111',
         '222222',
-        '333333',
-        '4444444'
+        '333333'
     ]},
     {type: 'image', value: './assets/image.png'}
 ]
